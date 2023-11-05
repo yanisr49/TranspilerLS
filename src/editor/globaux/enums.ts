b64b2a3663745b8eaffe87baad2b9815ab60ecd5
yanisr49 <9265588+yanisr49@users.noreply.github.com>
2023-11-05 03:19:15 +0100

    Update enums.ts

--- src/editor/globaux/enums.ts
+++ src/editor/globaux/enums.ts
@@ -1,17 +1,6 @@
-export enum CELL_TYPE {
-    EMPTY_CELL,
-    VOID_CELL,
-    OBSTACLE_CELL
-}
-
-export enum CAMP_TYPE {
-    ALLY,
-    ENEMY
-}
-
-export enum ENTITY_TYPE {
-    BULB,
-    CHEST,
-    LEEK,
-    TURRET
-}
+// Example
+// export enum CELL_TYPE {
+//     EMPTY_CELL,
+//     VOID_CELL,
+//     OBSTACLE_CELL
+// }
