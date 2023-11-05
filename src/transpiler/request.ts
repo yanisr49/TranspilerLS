818abfa6534cd669145faa0f9f4c59f1b3c4b8b4
yanis <yanisrichard@live.fr>
2023-11-05 16:17:00 +0100

    delete unused methods

--- src/transpiler/request.ts
+++ src/transpiler/request.ts
@@ -7,11 +7,6 @@ interface File {
     name: string;
 }
 
-export interface LeekIdAiId {
-    leekId: number;
-    aiId: number;
-}
-
 export default class API {
 
     private instance!: AxiosInstance;
@@ -39,28 +34,6 @@ export default class API {
         });
     }
 
-
-    public async createNewFolder(parentFolderId: number, name: string): Promise<number> {
-        setTimeout(() => {}, 1000);
-        return this.instance.post("/ai-folder/new-name", {
-            folder_id: parentFolderId,
-            name: name
-        }).then(data => {
-            return data.data.id;
-        });
-    }
-
-    public async createNewFile(parentFolderId: number, name: string): Promise<number> {
-        setTimeout(() => {}, 1000);
-        return this.instance.post("/ai/new-name", {
-            folder_id: parentFolderId,
-            version: 1,
-            name: name
-        }).then(data => {
-            return data.data.ai.id;
-        });
-    }
-
     public async updateCodeAI(fileAI: number, code: string) {
         setTimeout(() => {}, 1000);
         await this.instance.post("/ai/save", {
@@ -70,52 +43,4 @@ export default class API {
             // return data.data.id;
         });
     }
-
-    public async deleteFolder(folderId: number) {
-        setTimeout(() => {}, 1000);
-        await this.instance.delete(`/ai-folder/delete`, {
-            data: {folder_id: folderId},
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-        }).then(data => {
-            // return data.data.id;
-        });
-    }
-
-    public async getLeekAIName(leekId: number): Promise<string | undefined> {
-        setTimeout(() => {}, 1000);
-        return this.instance.get(`/leek/get/${leekId}`, {
-            data: {leek_id: leekId},
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-        }).then(data => {
-            return data.data.ai?.name;
-        });
-    }
-
-    public async getFarmerLeeks(farmerId: number): Promise<number[]> {
-        setTimeout(() => {}, 1000);
-        return this.instance.get(`/farmer/get/${farmerId}`, {
-            data: {farmer_id: farmerId},
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-        }).then(data => {
-            return Object.keys(data.data.farmer.leeks).map(id => +id);
-        });
-    }
-
-    public async setLeekAI(leekId: number, aiId: number): Promise<number[]> {
-        setTimeout(() => {}, 1000);
-        return this.instance.post(`/leek/ai`, {
-            leek_id: leekId,
-            ai_id: aiId
-        }).then(data => {
-            return Object.keys(data.data.farmer.leeks).map(id => +id);
-        });
-    }
-
-
 }
