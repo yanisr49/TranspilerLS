49ad94496fba3038c6169567bc9dd6f6f226a70a
yrichard <yanis.richard@capgemini.com>
2023-11-06 09:28:56 +0100

    update fichiers globaux

--- src/editor/globaux/enums.ts
+++ src/editor/globaux/enums.ts
@@ -1,7 +1,17 @@
-// Example : 
+export enum CELL_TYPE {
+    EMPTY_CELL,
+    VOID_CELL,
+    OBSTACLE_CELL
+}
 
-// export enum CELL_TYPE {
-//     EMPTY_CELL,
-//     VOID_CELL,
-//     OBSTACLE_CELL
-// }
+export enum CAMP_TYPE {
+    ALLY,
+    ENEMY
+}
+
+export enum ENTITY_TYPE {
+    BULB,
+    CHEST,
+    LEEK,
+    TURRET
+}
--- src/editor/main.ts
+++ src/editor/main.ts
@@ -1,1 +1,11 @@
-// Le code LS commence içi :)
+import {Fight} from "./fight/Fight";
+import {mark} from "./globaux/functions";
+import {COLOR_GREEN} from "./globaux/constants";
+
+
+export const FIGHT = new Fight();
+FIGHT.init();
+
+mark(FIGHT.cells[306].id, COLOR_GREEN, 1);
+
+console.log(FIGHT.cells[306].id)
