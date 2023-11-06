3e3c14ccf8c030aa372332635999b20b45194b9d
yanisr49 <9265588+yanisr49@users.noreply.github.com>
2023-11-06 09:38:37 +0100

    Update main.ts

--- src/editor/main.ts
+++ src/editor/main.ts
@@ -1,11 +1,1 @@
-import {Fight} from "./fight/Fight";
-import {mark} from "./globaux/functions";
-import {COLOR_GREEN} from "./globaux/constants";
-
-
-export const FIGHT = new Fight();
-FIGHT.init();
-
-mark(FIGHT.cells[306].id, COLOR_GREEN, 1);
-
-console.log(FIGHT.cells[306].id)
+// Le code LS commence içi :)
